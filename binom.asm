6beb715dca2c7e868c45083e44d9996846d41788
asaf weinberg <asafweinberg@mail.tau.ac.il>
2021-12-03 11:03:59 +0200

    fix some bugs

--- binom.asm
+++ binom.asm
@@ -4,7 +4,7 @@ FIB:
     sw $zero, &sp, &imm1, &s0, 1, 0
     sw $zero, &sp, &imm1, &s1, 0, 0
 
-    beq $zero, $a0, $imm1, imm2, 0, HALT_CONDITION
+    beq $zero, $a1, $zero, imm2, 0, HALT_CONDITION
     beq $zero, $a0, $a1, $imm2, 0, HALT_CONDITION
 
     add $s0, $a0, $zero, $zero, 0, 0
@@ -19,10 +19,11 @@ FIB:
     jal $ra, $zero, $zero, imm2, 0, FIB
 
     $add $v0, $v0, $s0, $zero, 0, 0
+	beq $zero, $zero, $zero, $imm2, 0, RETURN
 
 HALT_CONDITION:
     add $v0, $zero, $zero, $imm1, 1, 0
-
+RETURN:
     lw $ra, &sp, &imm1, &zero, 2, 0
     lw $s0, &sp, &imm1, &zero, 1, 0
     lw $s1, &sp, &imm1, &zero, 0, 0
